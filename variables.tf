8bb4a764b393f38921639d1e89fe3291947021cc
Omry Hay <46656490+omry-hay@users.noreply.github.com>
2022-10-31 23:26:50 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "client_name" {
   default = "Socks"
   description = "The Client Name"
-  type = "string"
+  type = string
 }
 
 variable "client_logo_url" {
   default = "https://www.jojomamanbebe.com/media/catalog/product/cache/1/image/1000x/cc44d3b39965d3efd15a8158cb2fdfb4/d/2/d2617redc4.jpg"
   description = "The Client Logo URL"
-  type = "string"
+  type = string
 }
